81a8b5dd09e14ef54e47a99728db93abe7a81f68
repirisyan <repirisyan@icloud.com>
2023-08-22 23:16:47 +0700

    add scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use App\Models\Project;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -13,6 +13,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Project::where('project_type','2')->where('project_status',true)->whereDate('until',date('Y-m-d'))->update([
+                'project_status' => false
+            ]);
+            Project::where('host_type','2')->where('contract_status',true)->whereDate('contract_until',date('Y-m-d'))->update([
+                'contract_status' => false
+            ]);
+        })->daily();
     }
 
     /**
